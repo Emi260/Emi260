0bf406c09fcad364e05e31b1b906680236c8422a
Emi260 <65983384+Emi260@users.noreply.github.com>
2021-04-15 15:09:38 -0300

    Update Person_company.dbo.sql

--- 00.CT/Person_company.dbo.sql
+++ 00.CT/Person_company.dbo.sql
@@ -26,3 +26,12 @@ create table client (
     address2 varchar(50),
     city varchar(30)
 )
+
+--changeset Emiliano:1
+create table client (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
