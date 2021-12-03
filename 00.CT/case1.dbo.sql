2e0da0d34684ef0af077fb15244030d7d3512d7e
Emiliano Quinto <equinto@baufest.com>
2021-12-03 18:07:31 -0300

    cambio en case 1

--- 00.CT/case1.dbo.sql
+++ 00.CT/case1.dbo.sql
@@ -2,21 +2,6 @@
 
 --changeset Emiliano:1
 create table users(
-  id int primary key,
-  name varchar2(30),
-  keys  varchar2(10)
-)
-
---changeset Emiliano:2
-Alter table users
-Add (
-date_add date,
-name varchar2(30)
-)
-
---changeset Emiliano:3
-Alter table usuarios
-Add 
-(
-date_update date
+  name varchar(30),
+  keys  varchar(10)
 )
