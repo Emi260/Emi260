4b5e354aef91c525f0be740bd66ebddd49934da3
Emiliano Quinto <equinto@baufest.com>
2021-12-06 16:06:23 -0300

    cambios en changelog

---
+++ 00.CT/Person_company.dbo.sql
@@ -0,0 +1,28 @@
+--liquibase formatted sql
+
+--changeset your.name:1
+create table person (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset your.name:2
+create table company (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset emiliano:3
+create table company (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50)
+    address2 varchar(50),
+    city varchar(30)
+)
