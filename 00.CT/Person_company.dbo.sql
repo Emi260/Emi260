b2c69db7812400119715e8e3ec98591b0b03f5e6
Emi260 <65983384+Emi260@users.noreply.github.com>
2021-12-29 16:37:01 -0300

    cambio en la creacion de la tabla compania

--- 00.CT/Person_company.dbo.sql
+++ 00.CT/Person_company.dbo.sql
@@ -22,7 +22,7 @@ create table company (
 create table compania (
     id int primary key,
     name varchar(50) not null,
-    address1 varchar(50)
+    address1 varchar(50),
     address2 varchar(50),
     city varchar(30)
 )
