3cb27eaae59c958d17fb21b5c27e6c49a35ef96b
Emi260 <65983384+Emi260@users.noreply.github.com>
2021-12-29 17:14:02 -0300

    4

--- 00.CT/tiendas.dbo.sql
+++ 00.CT/tiendas.dbo.sql
@@ -13,3 +13,6 @@ Add date_add date;
 
 --changeset copel:3
 insert into tiendas (id, name, responsable) values('1', 'Tienda1', 'Responsable1');
+
+--changeset copel:3
+DELETE FROM tiendas WHERE responsable='responsable1'; 
