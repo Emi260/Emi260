8c6cc42bec0533042eb3d03a8d34bd35019096b4
Emi260 <65983384+Emi260@users.noreply.github.com>
2021-12-29 17:02:33 -0300

    se modifica la tabla tiendas

--- 00.CT/tiendas.dbo.sql
+++ 00.CT/tiendas.dbo.sql
@@ -6,3 +6,10 @@ create table tiendas(
   name varchar(30),
   responsable varchar(30)
 )
+
+--changeset copel:2
+Alter table tiendas
+Add (date_add date,name varchar2(30));
+
+--changeset copel:3
+insert into tiendas (id, name, responsable, date) values('1', 'Tienda1', 'Responsable1', now);
