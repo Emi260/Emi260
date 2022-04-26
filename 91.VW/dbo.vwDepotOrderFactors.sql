44584dfc71e1b8521f8223d6156d8cf6576ae5f1
Emiliano Quinto <equinto@baufest.com>
2022-04-26 13:32:35 -0300

    update

--- 91.VW/dbo.vwDepotOrderFactors.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset none:none enddelimiter:\nGO runOnChange:true
-IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[vwDepotOrderFactors]'))
-EXEC dbo.sp_executesql @statement = N' 
-
-CREATE OR ALTER VIEW [dbo].[vwDepotOrderFactors] 
-AS  select Campo1=1' 
-GO
