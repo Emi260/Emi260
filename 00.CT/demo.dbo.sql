124b23a5e1fdbb3daefbadd2df42b5d3eb6eb2f8
Emiliano Quinto <equinto@baufest.com>
2022-04-27 17:36:57 -0300

    nueva tabla

--- 00.CT/demo.dbo.sql
+++ 00.CT/demo.dbo.sql
@@ -32,3 +32,14 @@ create table datos (
     city varchar(30)
 )
 --rollback DROP TABLE datos;
+
+--changeset jenkins:4 labels:sample-label context:sample-context
+--comment: se crea la tabla direcion
+create table direccion (
+    id int primary key not null identity(1, 1),
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+--rollback DROP TABLE direccion;
